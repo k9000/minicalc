4456db97a4b7fc316f7e4ac115f72a9a1d174333
k9000 <k9000@k10>
2012-12-23 01:23:17 +0900

    整数計算実装

--- src/com/tlulybluemonochrome/minimarucalc/MainActivity.java
+++ src/com/tlulybluemonochrome/minimarucalc/MainActivity.java
@@ -3,13 +3,21 @@ package com.tlulybluemonochrome.minimarucalc;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	double buf = 0;
+	double result = 0;
+	char calc = 1;
+	TextView text;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		text = (TextView) findViewById(R.id.editText1);
 	}
 
 	@Override
@@ -18,5 +26,105 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+	
+	public void clickButton0(View v){
+		buf = buf * 10 + 0;
+		text.setText(String.valueOf(buf));
+	}
+	
+	public void clickButton1(View v){
+		buf = buf * 10 + 1;
+		text.setText(String.valueOf(buf));
+	}
+	
+	public void clickButton2(View v){
+		buf = buf * 10 + 2;
+		text.setText(String.valueOf(buf));
+	}
+	
+	public void clickButton3(View v){
+		buf = buf * 10 + 3;
+		text.setText(String.valueOf(buf));
+	}
+	
+	public void clickButton4(View v){
+		buf = buf * 10 + 4;
+		text.setText(String.valueOf(buf));
+	}
+	
+	public void clickButton5(View v){
+		buf = buf * 10 + 5;
+		text.setText(String.valueOf(buf));
+	}
+	
+	public void clickButton6(View v){
+		buf = buf * 10 + 6;
+		text.setText(String.valueOf(buf));
+	}
+	
+	public void clickButton7(View v){
+		buf = buf * 10 + 7;
+		text.setText(String.valueOf(buf));
+	}
+	
+	public void clickButton8(View v){
+		buf = buf * 10 + 8;
+		text.setText(String.valueOf(buf));
+	}
+	
+	public void clickButton9(View v){
+		buf = buf * 10 + 9;
+		text.setText(String.valueOf(buf));
+	}
+	
+	public void clickButton_dot(View v){
 
+	}
+	
+	public void clickButton_add(View v){
+		result = calc(calc,buf,result);
+		calc = 1;
+		buf = 0;
+		text.setText(String.valueOf(result));
+	}
+	
+
+
+	public void clickButton_sub(View v){
+		result = calc(calc,buf,result);
+		calc = 2;
+		buf = 0;
+		text.setText(String.valueOf(result));
+	}
+	
+	public void clickButton_mul(View v){
+		result = calc(calc,buf,result);
+		calc = 3;
+		buf = 0;
+		text.setText(String.valueOf(result));
+	}
+
+	public void clickButton_dev(View v){
+		result = calc(calc,buf,result);
+		calc = 4;
+		buf = 0;
+		text.setText(String.valueOf(result));
+	}
+	
+	public void clickButton_equal(View v){
+		result = calc(calc,buf,result);
+		buf = 0;
+		text.setText(String.valueOf(result));
+	}
+	
+	private double calc(char a,double b,double c) {
+		double d = 0;
+
+		if(a==1)d = b + c;
+		else if(a==2)d = b -c;
+		else if(a==3)d = b * c;
+		else if(a==4)d = b / c;
+
+		return d;
+	}
 }
