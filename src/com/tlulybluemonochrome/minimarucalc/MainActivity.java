cbfa5614b9ff71b3568a8f9fb9f983ed3daa96f4
k9000 <k9000@k10>
2012-12-24 22:58:22 +0900

    レイアウト変更

--- src/com/tlulybluemonochrome/minimarucalc/MainActivity.java
+++ src/com/tlulybluemonochrome/minimarucalc/MainActivity.java
@@ -8,12 +8,12 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	double buf = 0;		//バッファ
-	double result = 0;	//計算結果
-	char calc = 1;		//四則演算の符号用
-	int dig = 0;		//小数点以下の桁数保持用
+	double buf = 0; // バッファ
+	double result = 0; // 計算結果
+	char calc = 1; // 四則演算の符号用
+	int dig = 0; // 小数点以下の桁数保持用
 	TextView text;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,129 +27,126 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-	
-	
+
 	/* クリア定義 */
-	public void clickButton_AC(View v){
+	public void clickButton_AC(View v) {
 		result = 0;
 		buf = 0;
 		dig = 0;
 		calc = 1;
 		text.setText(String.valueOf(buf));
 	}
-	
-	public void clickButton_C(View v){
+
+	public void clickButton_C(View v) {
 		buf = 0;
 		dig = 0;
 		text.setText(String.valueOf(buf));
 	}
-	
-	/*	数字ボタン定義 */
-	public void clickButton_figure(View v){
+
+	/* 数字ボタン定義 */
+	public void clickButton_figure(View v) {
 		int figure = 0;
 		// クリック時の処理
-		switch( v.getId()){
+		switch (v.getId()) {
 		case R.id.button1:
 			figure = 1;
-		break;
+			break;
 		case R.id.button2:
 			figure = 2;
-		break;
+			break;
 		case R.id.button3:
 			figure = 3;
-		break;
+			break;
 		case R.id.button4:
 			figure = 4;
-		break;
+			break;
 		case R.id.button5:
 			figure = 5;
-		break;
+			break;
 		case R.id.button6:
 			figure = 6;
-		break;
+			break;
 		case R.id.button7:
 			figure = 7;
-		break;
+			break;
 		case R.id.button8:
 			figure = 8;
-		break;
+			break;
 		case R.id.button9:
 			figure = 9;
-		break;
+			break;
 		case R.id.button0:
 			figure = 10;
-		break;
+			break;
 		}
-		
-		if(dig!=0){						//小数点計算
+
+		if (dig != 0) { // 小数点計算
 			dig = dig * 10;
-			buf = buf + ((double)figure/(double)dig);
-		}
-		else buf = buf * 10 + (double)figure;		//整数計算
+			buf = buf + ((double) figure / (double) dig);
+		} else
+			buf = buf * 10 + (double) figure; // 整数計算
 		text.setText(String.valueOf(buf));
 	}
-	
-	
+
 	/* 小数点ボタン定義 */
-	public void clickButton_dot(View v){
-		if(dig==0)dig = 1;
+	public void clickButton_dot(View v) {
+		if (dig == 0)
+			dig = 1;
 		text.setText(String.valueOf(buf));
 	}
-	
+
 	/* 演算ボタン定義 */
-	public void clickButton_calc(View v){
+	public void clickButton_calc(View v) {
 		// クリック時の処理
-		
-		//保持してる演算符号に従って演算
-		switch(calc){
+
+		// 保持してる演算符号に従って演算
+		switch (calc) {
 		case 1:
-			//加算
+			// 加算
 			result = result + buf;
 			break;
 		case 2:
-			//減算
+			// 減算
 			result = result - buf;
-		break;
+			break;
 		case 3:
-			//掛け算
+			// 掛け算
 			result = result * buf;
-		break;
+			break;
 		case 4:
-			//割り算
+			// 割り算
 			result = result / buf;
-		break;
+			break;
 		}
-		
-		buf = 0;//入力リセット
-		dig = 0;//小数点リセット
-		text.setText(String.valueOf(result));//結果表示
-		
-		//演算符号の保持
-		switch( v.getId()){
+
+		buf = 0;// 入力リセット
+		dig = 0;// 小数点リセット
+		text.setText(String.valueOf(result));// 結果表示
+
+		// 演算符号の保持
+		switch (v.getId()) {
 		case R.id.button_add:
-			//加算
+			// 加算
 			calc = 1;
 			break;
 		case R.id.button_sub:
-			//減算
+			// 減算
 			calc = 2;
-		break;
+			break;
 		case R.id.button_mul:
-			//掛け算
+			// 掛け算
 			calc = 3;
-		break;
+			break;
 		case R.id.button_div:
-			//割り算
+			// 割り算
 			calc = 1;
-		break;
+			break;
 		case R.id.button_equal:
-			//イコール
-			result = 0;//とりあえずリセット
-		break;
+			// イコール
+			result = 0;// とりあえずリセット
+			break;
 		}
-		
-		
 
 	}
-	
+
 }
